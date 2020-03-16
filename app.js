bdba02062097dad79b9c0e4f2fd5746eb647c807
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-03-16 07:04:57 +0600

    added mongoose

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 const app = express();
 
+dotenv.config()
+
+//Connect to Db
+mongoose.connect(
+process.env.DB_CONNECT,
+{ useUnifiedTopology: true, useNewUrlParser: true },
+    () => console.log('connected to Db'))
+
+// Router
+app.get('/user', (req, res) => {
+    res.json({ message: 'User' });
+})
 // port
 app.listen(5000,() => console.log('Server Up and Running'));
